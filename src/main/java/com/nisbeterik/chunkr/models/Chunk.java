3a06f854725863fdbac9f37ec7fb94deb2a4faa8
Erik Nisbet <eriknisbet8@gmail.com>
2024-03-18 20:55:02 +0100

    Created Chunk object

---
+++ src/main/java/com/nisbeterik/chunkr/models/Chunk.java
@@ -0,0 +1,13 @@
+package com.nisbeterik.chunkr.models;
+
+public class Chunk {
+
+    private String question;
+
+    private String answer;
+
+    public Chunk(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+}
