d5d145784a46f9f09644978c118cd16646e2933a
Erik Nisbet <eriknisbet8@gmail.com>
2024-03-28 10:52:12 +0100

    Assigned IDs and variables within ChunkController to UI components

--- src/main/java/com/nisbeterik/chunkr/ChunkController.java
+++ src/main/java/com/nisbeterik/chunkr/ChunkController.java
@@ -1,14 +1,27 @@
 package com.nisbeterik.chunkr;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class ChunkController {
     @FXML
-    private Label welcomeText;
+    private Label termLabel;
+    @FXML
+    private Label definitionLabel;
+    @FXML
+    private TextField termField;
+    @FXML
+    private TextField definitionField;
+    @FXML
+    private Button createChunkButton;
+
+
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void initialize() {
+
     }
 }
