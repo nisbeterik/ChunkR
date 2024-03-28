edc495002e4351471c722ea90e8f9ac0db7a4043
Erik Nisbet <eriknisbet8@gmail.com>
2024-03-28 11:06:06 +0100

    Implemented controller logic for creating new chunk object

--- src/main/java/com/nisbeterik/chunkr/ChunkController.java
+++ src/main/java/com/nisbeterik/chunkr/ChunkController.java
@@ -1,9 +1,11 @@
 package com.nisbeterik.chunkr;
 
+import com.nisbeterik.chunkr.models.Chunk;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class ChunkController {
     @FXML
@@ -17,11 +19,20 @@ public class ChunkController {
     @FXML
     private Button createChunkButton;
 
+    Chunk chunk;
+
 
 
 
     @FXML
     public void initialize() {
 
+    }
+
+    public void createChunk(MouseEvent mouseEvent) {
+        String term = termField.getText();
+        String definition = definitionField.getText();
+        chunk = new Chunk(term, definition);
+        System.out.println(chunk);
     }
 }
