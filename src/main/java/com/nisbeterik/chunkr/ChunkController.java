4efc374ec4467b32a4d02ca0c8fee2385dd03b67
Erik Nisbet <eriknisbet8@gmail.com>
2024-03-28 10:41:55 +0100

    Fixed UI componens in FXML file.

--- src/main/java/com/nisbeterik/chunkr/ChunkController.java
+++ src/main/java/com/nisbeterik/chunkr/ChunkController.java
@@ -3,7 +3,7 @@ package com.nisbeterik.chunkr;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class ChunkController {
     @FXML
     private Label welcomeText;
 
--- src/main/java/com/nisbeterik/chunkr/ChunkR.java
+++ src/main/java/com/nisbeterik/chunkr/ChunkR.java
@@ -3,18 +3,15 @@ package com.nisbeterik.chunkr;
 
 import com.nisbeterik.chunkr.Utils.FXMLUtil;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class ChunkR extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        final var hello = FXMLUtil.loadFxml("hello-view");
+        final var hello = FXMLUtil.loadFxml("create-chunk");
 
         final var scene = new Scene((Parent) hello);
         stage.setScene(scene);
